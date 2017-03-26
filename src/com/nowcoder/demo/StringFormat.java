abfa379013c845176b23160b32efc15e717aa3ac
pengmin <pengmin3@163.com>
2017-03-26 17:24:50 +0800

    字符串替换

---
+++ src/com/nowcoder/demo/StringFormat.java
@@ -0,0 +1,51 @@
+package com.nowcoder.demo;
+
+import org.junit.Test;
+
+/**
+ * 字符串替换
+ * 
+ * @author pengmin
+ *
+ */
+public class StringFormat {
+	public String formatString(String A, int n, char[] arg, int m) {
+		// write code here
+		StringBuffer stringBuffer = new StringBuffer();
+		// 先找到这个占位符一共有几个
+		int count = 0;
+		for (int i = 0, index = 0; i < n; i++) {
+			index = A.indexOf("%s", i);
+			if (index != -1) {
+				stringBuffer.append(A.substring(i, index));
+				stringBuffer.append(arg[count]);
+				i = index + 1;
+				count += 1;
+			} else {
+				stringBuffer.append(A.substring(i, n));
+				break;
+			}
+
+		}
+		// 占位符个数小于参数个数
+		if (count < m) {
+			for (int i = count; i < m; i++) {
+				stringBuffer.append(arg[i]);
+			}
+		}
+		System.out.println(stringBuffer);
+		String fomatString = stringBuffer.toString();
+		return fomatString;
+
+	}
+
+	@Test
+	public void testformatString() {
+		String A = "A%sC%sE";
+		int n = A.length();
+		char[] arg = { 'B', 'D', 'F' };
+		int m = arg.length;
+
+		formatString(A, n, arg, m);
+	}
+}
